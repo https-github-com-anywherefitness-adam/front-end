5bdd77f645ebf14ce3f865d47913991547f993fa
Blair <abrgroup@gmail.com>
2020-03-24 21:30:59 -0400

    added privateroute and added formik dependency

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./components/Login";
+import PrivateRoute from "./components/PrivateRoute";
+import ClassList from "./components/ClassList";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Anywhere Fitness App</h1>
+        </header>
+        <Route exact path="/" component={Login} />
+        <Switch>
+          <PrivateRoute exact path="/protected" component={ClassList} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
